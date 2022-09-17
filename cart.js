f4f7e9739da679ac68ebb2bb34ab86327cd70e80
OFENROHR100 <68556956+OFENROHR100@users.noreply.github.com>
2022-09-17 23:03:07 +0200

    Add files via upload

--- cart.js
+++ cart.js
@@ -111,7 +111,7 @@ function retriveData(str) {
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-      if (parseInt(this.responseText) > 12) {
+      if (parseInt(this.responseText) < 12) {
         document.getElementById(str).style.visibility = "hidden";
       }
       }
